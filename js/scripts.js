c6c8d6fdd5c6baf5fed02e08b89fbd78644a8c5f
Marco Barraza <marko.barraza@gmail.com>
2018-06-06 20:22:28 -0400

    se agregaron scripts finales

--- js/scripts.js
+++ js/scripts.js
@@ -12,19 +12,30 @@ $(function() {
 			+'<div class="publicaciones_icons">'
 			+'<i class="fas fa-trash-alt icons"></i>'
 			+'<i class="fas fa-heart icons"></i>'
+			+'<h2 id="contador">0</h2>'
 			+'</div>'
-			+'</div>')
+			+'</div>');
 	})
 
 	$('.contenedor_publicaciones').on('click', '.fa-trash-alt', function(){
-		$(this).parent().parent().fadeOut(400);
+		$(this).parent().parent().fadeOut(600);
 	})
 
 	$('.contenedor_publicaciones').on('click', '.fa-heart', function(e){
 		(e).stopPropagation();
 		(e).preventDefault();
+		var like = $(this).next().text();
+		var like = parseInt(like);
+		$(this).next().text(like+1);
 		$(this).toggleClass('heart-red');
 	})
 
+/*	$('.publicaciones_icons').on('click', '.fa-heart', function(e){
+		(e).stopPropagation();
+		(e).preventDefault();
+		var like = $(this).next().text();
+		var like = parseInt(like);
+		$('#contador').text(like+1);
+	})*/
 
 })
